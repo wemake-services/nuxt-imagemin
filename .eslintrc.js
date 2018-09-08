1f24d5798626d8f1f07877565c87a1c415f548de
sobolevn <mail@sobolevn.me>
2018-09-08 10:28:00 +0300

    Version 1.0.0 release
    Closes #15
    Closes #12
    Closes #2

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  'extends': [
+    'wemake',
+  ],
+  'env': {
+    'node': true,
+  },
+  'rules': {},
+  'parserOptions': {
+    'parser': 'babel-eslint',
+    'ecmaVersion': 2017,
+    'sourceType': 'module',
+  },
+}
